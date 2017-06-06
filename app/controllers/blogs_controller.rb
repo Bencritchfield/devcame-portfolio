dd71f8990e21117f71f1706d6b7220774a8c0e00
Ben Critchfield <ben.critchfield8@gmail.com>
2017-06-06 15:32:53 -0600

    Integrated new and created actions for portfolios

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -53,7 +53,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Post was deletedT.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
     end
   end
 
