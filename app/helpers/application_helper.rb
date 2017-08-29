97320c72f38abdcccd5d866a7f82fe850e48eb95
Ben Critchfield <ben.critchfield8@gmail.com>
2017-08-29 13:00:07 -0600

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,23 @@
 module ApplicationHelper
-	def login_helper style = ''
-		if current_user.is_a?(GuestUser)
-    (link_to "Register", new_user_registration_path, class: style) +
-       " ".html_safe +
+  def login_helper style = ''
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
-    else 
-     link_to "Logout", destroy_user_session_path, method: :delete, class: style
- 
-    end 
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
   end
 
   def source_helper(layout_name)
-  	if session[:source]
-  		greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-			 content_tag(:p, greeting, class: "source-greeting")
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
     end
+  end
+
+  def copyright_generator
+    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
   end
 
   def nav_items
@@ -38,7 +41,7 @@ module ApplicationHelper
       {
         url: portfolios_path,
         title: 'Portfolio'
-      }
+      },
     ]
   end
 
@@ -52,14 +55,20 @@ module ApplicationHelper
     nav_links.html_safe
   end
 
-
   def active? path
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Ben Critchfield's Portfolio", sticky: false)
+  end
+
 end
-
-
-
-
-
