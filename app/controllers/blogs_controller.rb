d9453d0abae8f38080c61cb76d2b98929f91a42b
Ben Critchfield <ben.critchfield8@gmail.com>
2017-10-30 16:17:45 -0600

    Added validations for topics in blogs and added  topic form element

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     if logged_in?(:site_admin)
-    @blogs = Blog.recent.page(params[:page]).per(5)
+    @blogs = Blog.recentj.page(params[:page]).per(5)
   else
     @blogs = Blog.published.recent.page(params[:page]).per(5)
   end
@@ -91,6 +91,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
 end
